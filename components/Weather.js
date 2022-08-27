8736b8254d25821884cd69f378269745adaed4e3
nuchnachaphonchoto <song290144@gmail.com>
2022-08-27 12:36:19 +0700

    checkpoint 2

---
+++ components/Weather.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Text,ImageBackground, StyleSheet, View } from 'react-native'
+import { useState } from 'react';
+import Forecast from './Forcast';
+
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+        }) 
+    return (
+        <View>
+            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </ImageBackground>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+});
+   
